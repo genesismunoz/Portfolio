23efa118f1c4d19adca0cb58a04236820e8255d3
Genesis Munoz <genesismunoz99@gmail.com>
2022-08-22 20:01:00 -0500

    Adding project.css and updating project.js

--- portfolio/src/pages/Projects.js
+++ portfolio/src/pages/Projects.js
@@ -1,10 +1,19 @@
 import React from "react";
 import ProjectItem from "../components/ProjectItem";
-//import Img
+import proj1 from '../assets/proj1.jpg'
+import proj2 from '../assets/proj2.jpg'
+import proj3 from '../assets/proj3.jpg'
 
 function Projects(){
     return (
-        <div>Projects
+        <div className="projects">
+            <h1> My Projects</h1>
+            <div className="projectList">
+                <ProjectItem name= "Garbage Route Optimizer" image={proj1} />
+                <ProjectItem name= "iPhone Multimeter" image={proj2} />
+                <ProjectItem name= "This Website" image={proj3} />
+
+            </div>
         </div>
     )
 }
