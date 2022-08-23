cefa961c67287867b0e28bd079cb2728211e3823
Genesis Munoz <genesismunoz99@gmail.com>
2022-08-22 20:36:01 -0500

    Added images to projects

--- portfolio/src/pages/Projects.js
+++ portfolio/src/pages/Projects.js
@@ -3,6 +3,7 @@ import ProjectItem from "../components/ProjectItem";
 import proj1 from '../assets/proj1.jpg'
 import proj2 from '../assets/proj2.jpg'
 import proj3 from '../assets/proj3.jpg'
+import '../styles/Projects.css'
 
 function Projects(){
     return (
