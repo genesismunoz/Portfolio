f8d250eb43fcdd5bbbad2a779a76922304d16f9d
Genesis Munoz <genesismunoz99@gmail.com>
2022-08-22 19:44:46 -0500

    Update Projects.js

---
+++ portfolio/src/pages/Projects.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ProjectItem from "../components/ProjectItem";
+//import Img
+
+function Projects(){
+    return (
+        <div>Projects
+        </div>
+    )
+}
+
+export default Projects;
